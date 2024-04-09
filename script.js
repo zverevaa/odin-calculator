3e74b8e39c846462b05ad1d048ee04b5060280d4
Kira Zvereva <angryvex@gmail.com>
2024-04-09 15:01:01 +0300

    Add float functionality

--- script.js
+++ script.js
@@ -12,8 +12,27 @@ const remove = document.querySelector(".rm-last");
 const negative = document.querySelector(".tgl-negative");
 
 const getDigit = (digit) => {
-    displayNum = makeDisplay(displayNum, digit.dataset.digit);
-    display.textContent = displayNum;
+    if (digit.dataset.digit === "." && display.textContent.includes(".")) {
+        return;
+    } else if (
+        digit.dataset.digit === "." &&
+        firstNum === null &&
+        display.textContent === "0"
+    ) {
+        displayNum = `0.`;
+        display.textContent = displayNum;
+    } else {
+        if (firstNum !== null && digit.dataset.digit === ".") {
+            displayNum = firstNum;
+        }
+        displayNum = makeDisplay(displayNum, digit.dataset.digit);
+        display.textContent = displayNum;
+        if (firstNum !== null && display.textContent.includes(".")) {
+            console.log(display.textContent);
+            firstNum = Number(display.textContent);
+            console.log(firstNum + "test");
+        }
+    }
 };
 
 const makeDisplay = (displayNum, digit) => {
@@ -32,15 +51,19 @@ clear.addEventListener("click", () => {
     firstNum = null;
     secondNum = null;
     display.classList.remove("you-died");
+    prevOperator = "";
+    operator = "";
 });
 
 //Remove last digit
+
 const removeLastDigit = () => {
-    if (firstNum === null) return;
-    displayNum = firstNum.toString();
+    if (display.textContent === "0") return; // Checks if there's anything to delete
+    if (firstNum && display.textContent === firstNum.toString()) return; // If there was a calculation, it doesn't let to edit the num
+    displayNum = display.textContent;
     displayNum = displayNum.slice(0, -1);
-    firstNum = parseInt(displayNum);
     display.textContent = displayNum;
+    if (display.textContent === "") display.textContent = "0";
 };
 
 remove.addEventListener("click", removeLastDigit);
@@ -51,11 +74,12 @@ const toggleNegative = () => {
 
     if (displayNum.includes("-")) {
         displayNum = displayNum.substring(1);
-        firstNum = parseInt(displayNum);
+        firstNum = Number(displayNum);
     } else {
         displayNum = `-${displayNum}`;
-        firstNum = parseInt(displayNum);
+        firstNum = Number(displayNum);
     }
+
     display.textContent = displayNum;
 };
 
@@ -89,11 +113,11 @@ const operate = (a, b, operator) => {
     if (a === null && operator === "=") return; //Checks if there's anything to calculate
 
     if (firstNum === null) {
-        firstNum = parseInt(displayNum);
+        firstNum = Number(displayNum);
         a = firstNum;
         clearDisplay();
     } else {
-        secondNum = parseInt(displayNum);
+        secondNum = Number(displayNum);
         b = secondNum;
     }
 
@@ -107,20 +131,20 @@ const operate = (a, b, operator) => {
     } else {
         switch (prevOperator) {
             case "+":
-                firstNum = add(a, b);
+                firstNum = Number(add(a, b).toFixed(4));
                 break;
             case "-":
-                firstNum = substract(a, b);
+                firstNum = Number(substract(a, b).toFixed(4));
                 break;
             case "*":
-                firstNum = multiply(a, b);
+                firstNum = Number(multiply(a, b).toFixed(4));
                 break;
             case "/":
                 if (secondNum === 0) {
                     firstNum = "YOU DIED";
                     display.classList.add("you-died");
                 } else {
-                    firstNum = divide(a, b);
+                    firstNum = Number(divide(a, b).toFixed(4));
                 }
                 break;
         }
